40e1602da060ba92a30ae14ac7a10a9390fcf5b1
Mike Mullen <m@m-mullen.com>
2013-12-15 22:08:44 -0800

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lakesofdesolation::Application.config.secret_key_base = '5583888f1b2dc0e2efac6462ed80709a10bccc2db76fff4dfdba1ad88f0832bc86bf1d4f8733ddc5f9497997f04150f03fdcb2d147e4b0add739f5d2c4c06e89'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lakesofdesolation::Application.config.session_store :cookie_store, key: '_lakesofdesolation_session'
