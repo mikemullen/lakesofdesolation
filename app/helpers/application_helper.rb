41dac489f2d17805cba36cfc9d847e28b40c53ef
Mike Mullen <m@m-mullen.com>
2014-08-17 16:08:48 -0700

    visual clean up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns full title for each page
   def full_title(page_title)
-  	base_title = "Lakes of Desolation"
+  	base_title = "The Lakes of Desolation Wilderness"
   	if page_title.empty?
   	  base_title
   	else
